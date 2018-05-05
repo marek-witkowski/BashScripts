de2f2e3d0c1251d132bafe698fd204e119726302
Marek Witkowski <marek.witkowski.priv@gmail.com>
2018-05-05 23:36:24 +0200

    Create tuneup_ubuntu_1804.sh

---
+++ tuneup_ubuntu_1804.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# install some tools
+
+sudo apt-get install vim mc htop
+
+# install java developmnet tools
+
+sudo apt-get install maven git git-flow gradle vagrant 
+
+# install Netbenas IDE
+
+sudo apt-get install netbeans
+
+
+# install slack
+
+sudo snap install slack --classic
+
+# install intelliJ 
+
+# sudo snap install intellij-idea-community --classic --edge
+sudo snap install intellij-idea-ultimate --classic --edgeedge
+
+
+
+
