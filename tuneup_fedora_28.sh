13d0a8fdd1c827a03d2e6942fba4f0a900036ef0
Marek Witkowski <marek.witkowski.priv@gmail.com>
2018-05-29 21:59:59 +0200

    Update tuneup_fedora_28.sh

--- tuneup_fedora_28.sh
+++ tuneup_fedora_28.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+
 # install some tools
 
 sudo dnf install vim mc htop
 
+
 # install java developmnet tools
 
 sudo dnf install maven git git-flow gradle vagrant 
+
+
+# install docker
+
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo  https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf install docker-ce
+
+
+
