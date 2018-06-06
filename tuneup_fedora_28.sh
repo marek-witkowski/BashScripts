ed81129829a74e577ae0b2ecb4cd8c012b416a82
Marek Witkowski <marek.witkowski.priv@gmail.com>
2018-06-06 18:38:49 +0200

    Update tuneup_fedora_28.sh

--- tuneup_fedora_28.sh
+++ tuneup_fedora_28.sh
@@ -15,7 +15,18 @@ sudo dnf install maven git git-flow gradle vagrant
 
 sudo dnf -y install dnf-plugins-core
 sudo dnf config-manager --add-repo  https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf config-manager --set-enabled docker-ce-edge
 sudo dnf install docker-ce
+
+sudo systemctl start docker
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+mkdir /home/$USER/.docker
+sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
+sudo chmod g+rwx "/home/$USER/.docker" -R
+
+sudo systemctl enable docker
 
 
 
