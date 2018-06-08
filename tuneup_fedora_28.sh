f9731b2601d37166a6db2666479f3db6c3f484c9
Marek Witkowski <marek.witkowski.priv@gmail.com>
2018-06-08 14:59:03 +0200

    Update tuneup_fedora_28.sh

--- tuneup_fedora_28.sh
+++ tuneup_fedora_28.sh
@@ -29,4 +29,15 @@ sudo chmod g+rwx "/home/$USER/.docker" -R
 sudo systemctl enable docker
 
 
+# install virtualbox
+
+sudo cd /etc/yum.repos.d/
+sudo wget http://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo
+sudo dnf update
+sudo dnf install binutils gcc make patch libgomp glibc-headers glibc-devel kernel-headers kernel-devel dkms
+sudo dnf install VirtualBox-5.2
+sudo /usr/lib/virtualbox/vboxdrv.sh setup
+sudo usermod -a -G vboxusers $USER
+
+
 
