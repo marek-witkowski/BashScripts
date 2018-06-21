abbf86395d0611de4dfef48305dc1e1622b191b1
Marek Witkowski <marek.witkowski.priv@gmail.com>
2018-06-21 21:46:34 +0200

    Update tuneup_ubuntu_1804.sh

--- tuneup_ubuntu_1804.sh
+++ tuneup_ubuntu_1804.sh
@@ -2,26 +2,34 @@
 
 # install some tools
 
-sudo apt-get install vim mc htop
+sudo apt-get install  -y vim mc htop
 
 # install java developmnet tools
 
-sudo apt-get install maven git git-flow gradle vagrant 
+sudo apt-get install -y  maven git git-flow gradle vagrant 
 
 # install Netbenas IDE
 
-sudo apt-get install netbeans
+sudo apt-get install  -y netbeans
 
 
 # install slack
 
-sudo snap install slack --classic
+sudo snap install  -y slack --classic
 
 # install intelliJ 
 
 # sudo snap install intellij-idea-community --classic --edge
-sudo snap install intellij-idea-ultimate --classic --edgeedge
+sudo snap install -y intellij-idea-ultimate --classic --edgeedge
 
 
+# install docker
 
+sudo apt-get install -y  apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
 
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt-get update
+sudo apt-get install -y docker-ce
