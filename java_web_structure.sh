c3b29cae526dd09efee09a32384b9644ea753a3a
Marek Witkowski <marek.witkowski.priv@gmail.com>
2018-08-06 12:42:35 +0200

    Update java_web_structure.sh

--- java_web_structure.sh
+++ java_web_structure.sh
@@ -2,18 +2,18 @@
 
 
 
-mkdir -p src/main/java 	        #   Application/Library sources
-mkdir -p src/main/resources     #	Application/Library resources
+mkdir -p src/main/java 	              #   Application/Library sources
+mkdir -p src/main/resources           #	Application/Library resources
 mkdir -p src/main/resources/META-INF
-mkdir -p src/main/filters 	    #   Resource filter files
-mkdir -p src/main/webapp 	    #   Web application sources
+mkdir -p src/main/filters 	          #   Resource filter files
+mkdir -p src/main/webapp 	            #   Web application sources
 mkdir -p src/main/webapp/WEB-INF
-mkdir -p src/test/java 	        #   Test sources
-mkdir -p src/test/resources 	#   Test resources
-mkdir -p src/test/filters 	    #   Test resource filter files
-mkdir -p src/it 	            #   Integration Tests (primarily for plugins)
-mkdir -p src/assembly 	        #   Assembly descriptors
-mkdir -p src/site #   Site
+mkdir -p src/test/java 	              #   Test sources
+mkdir -p src/test/resources 	        #   Test resources
+mkdir -p src/test/filters 	          #   Test resource filter files
+mkdir -p src/it 	                    #   Integration Tests (primarily for plugins)
+mkdir -p src/assembly 	              #   Assembly descriptors
+mkdir -p src/site                     #   Site
 
 touch src/main/resources/META-INF/presistence.xml
 touch src/main/resources/log4j2.xml
