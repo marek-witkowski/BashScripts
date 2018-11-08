719e455039f91b0834eeb08e1ced2d92bf6a1318
Marek Witkowski <marek.witkowski.priv@gmail.com>
2018-11-08 11:39:39 +0100

    Update tuneup_fedora_28.sh

--- tuneup_fedora_28.sh
+++ tuneup_fedora_28.sh
@@ -8,7 +8,7 @@ sudo dnf -y install vim mc htop chrome-gnome-shell libappindicator gnome-tweak-t
 
 # install java developmnet tools
 
-sudo dnf -y install maven git gitflow gradle vagrant wildfly java-8 java-11
+sudo dnf -y install maven git gitflow gradle vagrant wildfly java-8 java-11 gitk
 
 
 # install docker
