e057b61845ba0abd31b271b0008b5b61150f5381
Marek Witkowski <marek.witkowski.priv@gmail.com>
2018-11-15 13:41:27 +0100

    first working version

--- junkMail2BlackList.sh
+++ junkMail2BlackList.sh
@@ -1,3 +1,35 @@
 #!/bin/bash
 
+echo "JunkMail 2 BlackList"
+echo "--------------------"
 
+touch results.txt
+
+path2mail="/home"
+path2junk1="/Maildir/.Junk/cur"
+path2junk2="/Maildir/.Niechciane/cur"
+
+for path in $path2mail/* ;  do
+
+        if [[ -d "$path" && ! -L "$path" ]]; then
+
+                fullPath1="$path$path2junk1";
+                fullPath2="$path$path2junk2";
+
+                if [ -d  "$fullPath1" ]; then
+                echo "$fullPath1";
+                        grep "From: "  $fullPath1/* >> temp.txt;
+                fi;
+                
+                if [ -d "$fullPath2" ]; then
+                echo "$fullPath2";
+                        grep "From: "  $fullPath2/* >> temp.txt;
+                fi;
+
+
+        fi;
+
+done
+
+
+awk -F "<" '{print $2}' temp.txt | sed s'/>//' | awk 'NF' | awk -F "@" '{print $2}' | sort -u  > results.txt;
