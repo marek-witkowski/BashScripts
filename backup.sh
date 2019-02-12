7f914919a18616a116ea6dec47272264bdc8c2af
Marek Witkowski <marek.witkowski.priv@gmail.com>
2019-02-12 14:28:31 +0100

    some changes in backup.sh

--- backup.sh
+++ backup.sh
@@ -2,9 +2,7 @@
 
 # -------------------------------------------------------------------
 # Simple backup script
-#
 # author: Marek Witkowski (http://www.github.com/marekwu)
-#
 # -------------------------------------------------------------------
 
 data_old=`date --date='7 days ago'  +%Y-%m-%d`
@@ -15,49 +13,36 @@ data=`date +%Y-%m-%d`
 # --------------------------------------------------------------------
 
 cd /
-
         tar -cvvf $data-etc.tar.gz etc/
         mv $data-etc.tar.gz /backup/server/etc
-
 cd /var/log
-
         tar -cvvf $data-maillog.tar.gz maillog
         mv $data-maillog.tar.gz /backup/server/log/
-
 
 # --------------------------------------------------------------------
 # backup /home
 # --------------------------------------------------------------------
 
 cd /backup/server/
+	mkdir $data
+	cd $data
+	mkdir home
+	cd /home
 
-mkdir $data
+	rm *.gz
 
-cd $data
-
-mkdir home
-
-cd /home
-
-rm *.gz
-
-
-for i in `ls`
-
-        do
-                tar -cvvf $data-$i.tar.gz $i/
-                mv $data-$i.tar.gz /backup/server/$data/home
-                rm *.gz
-        done
-
+	for i in `ls`
+        	do
+                	tar -cvvf $data-$i.tar.gz $i/
+	                mv $data-$i.tar.gz /backup/server/$data/home
+        	        rm *.gz
+	        done
 
 # --------------------------------------------------------------------
 # remove old backup
 # --------------------------------------------------------------------
 
-
 rm /backup/server/etc/$data_old-etc.tar.gz
 rm /backup/server/$data_old-maillog.tar.gz
-
 rm -r -f /backup/server/$data_old
 
