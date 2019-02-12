b9aea5d5e3175ad449c7fd7bdde5560122575dc7
Marek Witkowski <marek.witkowski.priv@gmail.com>
2019-02-12 14:37:14 +0100

    added actual data to filename

--- printers.sh
+++ printers.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-report="report.csv"
+today=`date +%Y-%m-%d`
+reportfile=$today"_printers_report.csv"
 
 
 printers=("192.168.3.46" "192.168.3.77" "192.168.3.79" "192.168.3.85")
 
 
-echo "Printer; counter" | tee >> $report
+echo "Printer; counter" | tee >> $reportfile
 
 for count in ${printers[*]}
 do	
-	echo -n $count";" | tee >> $report
-	snmpget  -c public  -v 2c $count 1.3.6.1.2.1.43.10.2.1.4.1.1 | awk '{print $NF}' | tee >> $report 
+	echo -n $count";" | tee >> $reportfile
+	snmpget  -c public  -v 2c $count 1.3.6.1.2.1.43.10.2.1.4.1.1 | awk '{print $NF}' | tee >> $reportfile
 
 done
