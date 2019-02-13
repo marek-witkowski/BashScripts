e8ab3a77b38695587933f868e938daccfd68127b
Marek Witkowski <marek.witkowski.priv@gmail.com>
2019-02-13 12:52:07 +0100

    added reading printers from file

--- printers/report.sh
+++ printers/report.sh
@@ -4,12 +4,10 @@ today=`date +%Y-%m-%d`
 reportfile=$today"_printers_report.csv"
 
 
-printers=("192.168.3.46" "192.168.3.77" "192.168.3.79" "192.168.3.85")
 
+echo "Printer; counter" | tee > $reportfile
 
-echo "Printer; counter" | tee >> $reportfile
-
-for count in ${printers[*]}
+for count in $(cat printers.txt) 
 do	
 	echo -n $count";" | tee >> $reportfile
 	snmpget  -c public  -v 2c $count 1.3.6.1.2.1.43.10.2.1.4.1.1 | awk '{print $NF}' | tee >> $reportfile
