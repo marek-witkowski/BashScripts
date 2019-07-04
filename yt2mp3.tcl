d30f67c1329a59a9a1a33984410351a05bfc3bfb
Marek Witkowski <marekwu@localhost.localdomain>
2019-07-04 21:59:32 +0200

    yt2mp3.tcl added.

---
+++ yt2mp3.tcl
@@ -0,0 +1,13 @@
+#!/usr/bin/wish
+
+
+grid [label .myLabel -text "Sciezka zapisu:"]
+grid [entry .pathEntry -text "/home/mareku/Muzyka"]
+
+grid [label .labMusic -text "URL klipu:"]
+grid [entry .urlEntry]
+
+
+grid [button .btnExtract -text "Sciagnij" ]
+grid [button .btnExit -text "Zakoncz"] 
+
