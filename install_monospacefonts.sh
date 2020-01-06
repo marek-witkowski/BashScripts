e2fbf0752a8b252f4b5b910c57da2eaf7d8fa823
Marek Witkowski <marek.witkowski.priv@gmail.com>
2020-01-06 15:58:06 +0100

    Update install_monospacefonts.sh

--- install_monospacefonts.sh
+++ install_monospacefonts.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# Script to install great monospaced fonts for code and terminal in Fedora.
+
 sudo dnf install -y levien-inconsolata-fonts adobe-source-code-pro-fonts mozilla-fira-mono-fonts google-droid-sans-mono-fonts dejavu-sans-mono-fonts
