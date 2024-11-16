2acd2832e1844e102524cf39b39d134598618688
Marek Witkowski <marek.witkowski.priv@gmail.com>
2024-11-16 10:51:42 +0100

    Update jpg2webp.sh

--- jpg2webp.sh
+++ jpg2webp.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
 find -name '*.jpg' -exec mogrify -format webp -quality 75 {} \;
+find -name '*.jpeg' -exec mogrify -format webp -quality 75 {} \;
+find -name '*.png' -exec mogrify -format webp -quality 75 {} \;
+
+kdialog --msgbox 'Finished!'
